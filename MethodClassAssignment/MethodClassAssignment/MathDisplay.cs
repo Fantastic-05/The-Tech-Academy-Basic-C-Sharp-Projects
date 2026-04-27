de72fdd7d6ae57ebd775fc9116a33047571757b6
Fantastic-05 <hibakhan19@hotmail.com>
2026-04-27 11:57:25 -0400

    MethodClassAssignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// This class contains a void method that performs a math operation
+public class MathDisplay
+{
+    // This method takes two integers as parameters
+    // It performs a math operation on the first number
+    // and displays the second number to the console
+    public void DoMath(int num1, int num2)
+    {
+        // Perform a math operation on the first number (example: multiply by 2)
+        int result = num1 * 2;
+
+        // Display the result of the math operation
+        Console.WriteLine("Result of first number * 2: " + result);
+
+        // Display the second number
+        Console.WriteLine("Second number is: " + num2);
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate (create an object of) the MathDisplay class
+        MathDisplay math = new MathDisplay();
+
+        // Call the method by passing arguments normally
+        math.DoMath(5, 10);
+
+        Console.WriteLine(); // blank line for readability
+
+        // Call the method using named parameters
+        math.DoMath(num1: 7, num2: 20);
+
+        // Keeps console window open until user presses a key
+        Console.ReadLine();
+    }
+}
