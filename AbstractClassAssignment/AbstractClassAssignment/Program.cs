7927a772340490bbe5da639337561b29e7883ca6
Fantastic-05 <hibakhan19@hotmail.com>
2026-04-28 17:41:29 -0400

    AbstractClassAssignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main method, instantiate an Employee object
+            Employee sam = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            //Call the SayName method on the object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
