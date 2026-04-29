6b4bab9a79dfeb7becedaaae3e7617c2c217a42c
Fantastic-05 <hibakhan19@hotmail.com>
2026-04-29 17:01:23 -0400

    ParametersAssignment

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Generic Employee class (T can be any data type)
+public class Employee<T>
+{
+    // Property called "Things" which is a list of type T
+    public List<T> Things { get; set; }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // -----------------------------------------
+        // Employee with string type
+        // -----------------------------------------
+
+        // Create an Employee object that holds strings
+        Employee<string> stringEmployee = new Employee<string>();
+
+        // Assign a list of strings to Things property
+        stringEmployee.Things = new List<string>
+        {
+            "Laptop",
+            "Phone",
+            "Notebook"
+        };
+
+        // -----------------------------------------
+        // Employee with int type
+        // -----------------------------------------
+
+        // Create an Employee object that holds integers
+        Employee<int> intEmployee = new Employee<int>();
+
+        // Assign a list of integers to Things property
+        intEmployee.Things = new List<int>
+        {
+            10,
+            20,
+            30
+        };
+
+        // -----------------------------------------
+        // Print all items (strings)
+        // -----------------------------------------
+
+        Console.WriteLine("String Employee Things:");
+        foreach (string item in stringEmployee.Things)
+        {
+            Console.WriteLine(item);
+        }
+
+        // -----------------------------------------
+        // Print all items (integers)
+        // -----------------------------------------
+
+        Console.WriteLine("\nInteger Employee Things:");
+        foreach (int item in intEmployee.Things)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Keep console open
+        Console.ReadLine();
+    }
+}
