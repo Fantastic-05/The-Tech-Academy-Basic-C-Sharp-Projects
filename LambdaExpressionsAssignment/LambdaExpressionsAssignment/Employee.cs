f004bd99c2c4398ed3cfff6e3626a0ecb2ae4217
Fantastic-05 <hibakhan19@hotmail.com>
2026-04-29 17:37:26 -0400

    LambdaExpressionsAssignment

---
+++ LambdaExpressionsAssignment/LambdaExpressionsAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExpressionAssignment
+{
+    //Create an Employee class with id, firstname and lastname properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
