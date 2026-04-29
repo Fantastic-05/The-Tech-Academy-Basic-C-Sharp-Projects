f49f928f0e8c0f4bd41b662063f18ef5106786d2
Fantastic-05 <hibakhan19@hotmail.com>
2026-04-29 17:26:34 -0400

    Struct Assignment

---
+++ Struct Assignment/Struct Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Define a struct called Number
+// A struct is a value type used to store small data objects
+public struct Number
+{
+    // Property called Amount of type decimal
+    public decimal Amount { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create an object of type Number
+        Number myNumber = new Number();
+
+        // Assign a value to the Amount property
+        myNumber.Amount = 123.45m; // 'm' indicates a decimal value
+
+        // Print the value to the console
+        Console.WriteLine("The amount is: " + myNumber.Amount);
+
+        // Keep console window open until user presses Enter
+        Console.ReadLine();
+    }
+}
